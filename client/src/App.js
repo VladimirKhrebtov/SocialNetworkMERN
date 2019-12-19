ba0ba77adbe894cf2d1942ae053b68e18bf3d382
VladimirKhrebtov <vladimir.h@belvg.com>
2019-12-19 10:55:13 +0300

    private router for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/layout/Register";
-import Login from "./components/layout/Login";
-import Alert from "./components/layout/Alert";
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/layout/Register';
+import Login from './components/layout/Login';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
-import { loadUser } from "./actions/auth";
+import { loadUser } from './actions/auth';
 import store from './store';
 import './App.css';
-import setAuthToken from "./utils/setAuthToken";
+import setAuthToken from './utils/setAuthToken';
 
 if(localStorage.token) {
     setAuthToken(localStorage.token);
@@ -27,11 +29,12 @@ const App = () => {
                 <Fragment>
                     <Navbar/>
                     <Route exact path='/' component={Landing} />
-                    <section className="container">
+                    <section className='container'>
                         <Alert/>
                         <Switch>
                             <Route exact path='/register' component={Register} />
-                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/login' component={Login} />'
+                            <PrivateRoute exact path='/dashboard' component={Dashboard}></PrivateRoute>
                         </Switch>
                     </section>
                 </Fragment>
